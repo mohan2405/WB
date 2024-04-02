47d9a74a10b1670e428f6ad98d1cb1695560f55f
mohan2405 <mohan24005@gmail.com>
2024-04-02 23:12:43 +0530

    ready

--- config.py
+++ config.py
@@ -1,15 +1,19 @@
-mobile = "7975580881"
-no_otp = 0
-time_s = 180
-
-
 # Configurations
+
 MOBILE_NUMBER = "8078644957"
-PASSWORD = "Qwerty@12345!@#$%"
-CNF_PASSWORD = "Qwerty@12345!@#$%"
-URL = "https://88panel.com/forgot-password/winbuzz.com"
-WITHDRAW_URL = "https://88panel.com/withdraw/winbuzz.in/1812082/dkzDiyLZHUXG4kv3c7c08daa3cee5bedd7ed8832b0a7bb25"
+NO_OTP = 0
+TIME_S = 180
+
+PASSWORD = "QWERTY@12345!@#$%"
+CNF_PASSWORD = "QWERTY@12345!@#$%"
+
 TOKEN = "j74yR2sZDRjhRmtL9unz8ZPSJlCFtVnXKtA1Wx8Y"
+
+URL = "https://88panel.com/forgot-password/winbuzz.com"
+
+# WITHDRAW_URL = "https://88panel.com/withdraw-password-client/reset/863891/kUH4LkBdmJxbpBrHd3b0463a4d5cee4d1d7770e5653d8139"
+
+# LOGIN_URL = "https://88panel.com/forgot-password/winbuzz.com"
 
 
 R1 = 1000
@@ -22,3 +26,32 @@ R7 = 7000
 R8 = 8000
 R9 = 9000
 R10 = 9999
+
+
+# To Run program on 2 PCs
+
+# For 1st PC
+
+# R1 = 500
+# R2 = 1000
+# R3 = 1500
+# R4 = 2000
+# R5 = 2500
+# R6 = 3000
+# R7 = 3500
+# R8 = 4000
+# R9 = 4500
+# R10 = 5000
+
+# For 2nd PC
+
+# R1 = 5500
+# R2 = 6000
+# R3 = 6500
+# R4 = 7000
+# R5 = 7500
+# R6 = 8000
+# R7 = 8500
+# R8 = 9000
+# R9 = 9500
+# R10 = 9999
