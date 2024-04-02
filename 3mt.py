768373c020403a89f9ede17f54e1d6d01230cc1a
mohan2405 <mohan24005@gmail.com>
2024-04-02 22:36:20 +0530

    var name changed

--- 3mt.py
+++ 3mt.py
@@ -6,10 +6,10 @@ import config
 def send_request():
     global config
     url = "https://88panel.com/forgot_otp/winbuzz.com"
-    data = {"mobile": config.mobile, "_token": "OGcZZi42toVjDhacbWy7N2BBizJ6rd1hG2dNZgOO"}
+    data = {"mobile": config.MOBILE, "_token": "OGcZZi42toVjDhacbWy7N2BBizJ6rd1hG2dNZgOO"}
     response = requests.post(url, data=data)
     if response.status_code == 200:
-        config.no_otp += 1
+        config.NO_OTP += 1
         print(config.no_otp, "Request sent with status code:", response.status_code)
     else:
         print("Error Occurred...!!!:", response.status_code)
@@ -18,5 +18,5 @@ def send_request():
 send_request()
 
 while True:
-    time.sleep(config.time_s)
+    time.sleep(config.TIME_S)
     send_request()
--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-mobile = "7829862342"
-no_otp = 0
-time_s = 180
+MOBILE = "7829862342"
+NO_OTP = 0
+TIME_S = 180
